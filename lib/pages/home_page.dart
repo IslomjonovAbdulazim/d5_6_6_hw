68338f9f2f3a129b9af8f3849619dec7c95eea5e
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-25 16:59:51 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:d5_6_6_hw/pages/start_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
@@ -19,6 +23,38 @@ class HomePage extends StatelessWidget {
                 // Texts (3x)
 
                 // Button
+                CupertinoButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => StartPage(),
+                      ),
+                    );
+                  },
+                  color: Color(0xffFE6F23),
+                  borderRadius: BorderRadius.circular(20),
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Get Started",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Icon(
+                        Icons.keyboard_double_arrow_right,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
 
                 // Text
               ],
